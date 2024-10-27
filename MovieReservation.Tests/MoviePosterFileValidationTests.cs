4d2c2ea0ec86954e6a3d3b6b7e872a4150c0a415
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2024-10-26 20:24:43 -0500

    added code for MoviePosterFileAttribute unit tests

--- MovieReservation.Tests/MoviePosterFileValidationTests.cs
+++ MovieReservation.Tests/MoviePosterFileValidationTests.cs
@@ -1,7 +1,49 @@
-﻿
+﻿using Microsoft.AspNetCore.Http;
+using MovieReservation.ViewModels;
+using System.Text;
+
 namespace MovieReservation.Tests
 {
-    internal class MoviePosterFileValidationTests
+    public class MoviePosterFileValidationTests
     {
+        [Theory]
+        [InlineData("valid-test-file-1.jpeg", 30 * 1024, true)]
+        [InlineData("valid-test-file-2.png", 30 * 1024, true)]
+        [InlineData("invalid-test-file-1.txt", 30 * 1024, false)]
+        [InlineData("invalid-test-file-2.jpg", 11 * (1024 * 1024), false)]
+        public void TestWithFormFile(string fileName, int fileSize, bool expected)
+        {
+            byte[] fileBytes = Encoding.UTF8.GetBytes(fileName);
+            IFormFile file = new FormFile(baseStream: new MemoryStream(fileBytes), 
+                baseStreamOffset: 0, 
+                name: "posterImage", 
+                fileName: fileName, 
+                length: fileSize);
+
+            MoviePosterFileAttribute attribute = new();
+            bool result = attribute.IsValid(file);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestWithInvalidType()
+        {
+            int test = 0;
+            MoviePosterFileAttribute attribute = new();
+
+            bool result = attribute.IsValid(test);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TestWithNull()
+        {
+            MoviePosterFileAttribute attribute = new();
+            bool result = attribute.IsValid(null);
+
+            Assert.False(result);
+        }
     }
 }
