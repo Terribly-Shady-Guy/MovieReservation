c089491fc2cdf2c4093dbe4d20718e95c276cdd0
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-02-11 18:44:47 -0600

    added regex for validating password

--- MovieReservation/ViewModels/NewUserVM.cs
+++ MovieReservation/ViewModels/NewUserVM.cs
@@ -9,6 +9,8 @@ namespace MovieReservation.ViewModels
         [EmailAddress]
         public required string Email { get; set; }
         public required string Username { get; set; }
+        [RegularExpression(@"^(?:(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%&*+=]))[\da-zA-Z!@#$%&*+=]{8,}$",
+            ErrorMessage = "Password does not contain an upercase letter, lowercase letter, number, or special character, or is not at least 8 characters long.")]
         public required string Password { get; set; }
     }
 }
