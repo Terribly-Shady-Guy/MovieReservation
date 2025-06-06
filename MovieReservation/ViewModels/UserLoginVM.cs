a9369888ee7abc16cae6b0c74f96e448e4427d20
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-06-05 21:08:24 -0500

    added validation for password for login

--- MovieReservation/ViewModels/UserLoginVM.cs
+++ MovieReservation/ViewModels/UserLoginVM.cs
@@ -6,6 +6,7 @@ namespace MovieReservation.ViewModels
     {
         [EmailAddress]
         public required string Email { get; set; }
+        [MinLength(8)]
         public required string Password { get; set; }
     }
 }
