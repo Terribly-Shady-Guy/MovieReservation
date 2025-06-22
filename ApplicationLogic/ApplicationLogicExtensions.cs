904560a3eea68fb14385ab4dcd0da8b52bdb42e5
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-06-21 22:38:33 -0500

    added doc for application logic extension

--- ApplicationLogic/ApplicationLogicExtensions.cs
+++ ApplicationLogic/ApplicationLogicExtensions.cs
@@ -9,6 +9,13 @@ namespace ApplicationLogic
 {
     public static class ApplicationLogicExtensions
     {
+        /// <summary>
+        /// Adds services for core movie reservation logic.
+        /// </summary>
+        /// <param name="services">The service collection instance for the application.</param>
+        /// <param name="config">The application configuration object.</param>
+        /// <param name="environment">The application environment config.</param>
+        /// <returns></returns>
         public static IServiceCollection AddApplicationLogicServices(this IServiceCollection services, IConfiguration config, IWebHostEnvironment environment)
         {
             services.AddTransient<AuthenticationService>();
