e63ee211f1f407bcac3838b4d1a2eb94a68de558
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-07-02 07:33:06 -0500

    rearranged code order to only get file stream when content type is supported

--- MovieReservation/Program.cs
+++ MovieReservation/Program.cs
@@ -43,13 +43,6 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/Images/{fileName}", Results<FileStreamHttpResult, NotFound<string>, BadRequest<string>>(string fileName, [FromServices] IFileHandler handler) =>
 {
-    var result = handler.GetFile(fileName);
-
-    if (!result.Success || result.FileStream is null)
-    {
-        return TypedResults.NotFound("File does not exist.");
-    }
-
     var contentTypes = new Dictionary<string, string>
     {
         [".jpg"] = "image/jpeg",
@@ -57,11 +50,18 @@ app.MapGet("/Images/{fileName}", Results<FileStreamHttpResult, NotFound<string>,
         [".png"] = "image/png"
     };
 
-    string? ext = Path.GetExtension(result.FileName);
+    string? ext = Path.GetExtension(fileName);
 
     if (ext is null || !contentTypes.TryGetValue(ext, out string? contentType))
     {
         return TypedResults.BadRequest("This is not a supported file type.");
+    }
+
+    var result = handler.GetFile(fileName);
+
+    if (!result.Success || result.FileStream is null)
+    {
+        return TypedResults.NotFound("File does not exist.");
     }
 
     return TypedResults.File(result.FileStream, contentType, result.FileName);
