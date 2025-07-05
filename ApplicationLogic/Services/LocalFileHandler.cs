c50ad60bf005a35cbf4f4547ea4f6334ee62ece1
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-07-05 15:27:03 -0500

    changed template naming to follow recommendations by serilog

--- ApplicationLogic/Services/LocalFileHandler.cs
+++ ApplicationLogic/Services/LocalFileHandler.cs
@@ -2,6 +2,7 @@
 using ApplicationLogic.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.Xml;
 
 namespace ApplicationLogic.Services
 {
@@ -53,7 +54,7 @@ namespace ApplicationLogic.Services
             {
                 if (ex is UnauthorizedAccessException || ex is IOException)
                 {
-                    _logger.LogWarning(ex, "Exception handled but logged for possible issue. file name: {fileName} at {filePath} in {fileHandler}", fileName, _path, nameof(LocalFileHandler));
+                    _logger.LogWarning(ex, "Exception handled but logged for possible issue. file name: {FileName} at {FilePath} in {FileHandler}", fileName, _path, nameof(LocalFileHandler));
                     return FileHandlerResult.Failed();
                 }
                 else
