875a0d2478b100f083d921d967462c75058ed76a
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-08-21 22:42:33 -0500

    removed title modification for deprecation to rely on warning

--- MovieReservation/OpenApi/Transformers/VersionedDocumentTransformer.cs
+++ MovieReservation/OpenApi/Transformers/VersionedDocumentTransformer.cs
@@ -25,8 +25,7 @@ namespace MovieReservation.OpenApi.Transformers
 
             if (description.IsDeprecated)
             {
-                document.Info.Title += " (DEPRECATED)";
-                document.Info.Description += $" \n> [!warning] This version of the API is deprecated and {(description.SunsetPolicy is not null ? $"will be removed on **{description.SunsetPolicy.Date}**" : "may be removed in the future")}.";
+                document.Info.Description += $"\n> [!warning] This version of the API is deprecated and {(description.SunsetPolicy is not null ? $"will be removed on **{description.SunsetPolicy.Date}**" : "may be removed in the future")}.";
             }
             
             document.Components ??= new OpenApiComponents();
