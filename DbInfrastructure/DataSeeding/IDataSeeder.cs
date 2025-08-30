a2705a9448580e89ff08e3f77befa1a6a5bf6970
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-08-30 18:35:26 -0500

    added docs for IDataSeeder

--- DbInfrastructure/DataSeeding/IDataSeeder.cs
+++ DbInfrastructure/DataSeeding/IDataSeeder.cs
@@ -2,9 +2,24 @@
 
 namespace DbInfrastructure.DataSeeding
 {
+    /// <summary>
+    /// A contract for creating a data seeding module. Implement this interface and register the implementation as a scoped service to allow visibility to the seeding provider. 
+    /// Both sync and async must be implemented to support flexible execution.
+    /// </summary>
     public interface IDataSeeder
     {
-        public void Add(DbContext context);
-        public Task AddAsync(DbContext context, CancellationToken cancellationToken);
+        /// <summary>
+        /// Seeds data synchronously.
+        /// </summary>
+        /// <param name="context">The <see cref="DbContext"/> instance used to apply seeding operations.</param>
+        void Add(DbContext context);
+
+        /// <summary>
+        /// Seeds data asynchronously.
+        /// </summary>
+        /// <param name="context">The <see cref="DbContext"/> instance used to apply seeding operations.</param>
+        /// <param name="cancellationToken">The token used to observe cancellation requests for asynchronous operations.</param>
+        /// <returns>A <see cref="Task" /> that is resolved when the asynchronous execution finishes.</returns>
+        Task AddAsync(DbContext context, CancellationToken cancellationToken);
     }
 }
