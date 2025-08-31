0119c38d15ed799a58097baf1036668ff30f04ae
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-08-30 22:18:31 -0500

    finished setting up application factory for testing

--- Tests/Integration/MovieReservationApplicationFactory.cs
+++ Tests/Integration/MovieReservationApplicationFactory.cs
@@ -1,18 +1,26 @@
 ﻿using DbInfrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Tests.Integration
 {
-    internal class MovieReservationApplicationFactory : WebApplicationFactory<Program>
+    internal class MovieReservationWebApplicationFactory : WebApplicationFactory<Program>
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) =>
             {
+                services.RemoveAll<DbContextOptions<MovieReservationDbContext>>();
                 services.AddDbInfrastructure(context.Configuration.GetConnectionString("testing"));
             });
+
+            builder.UseEnvironment("Development");
         }
     }
+
+    [CollectionDefinition]
+    public class WebApplicationFactoryCollectionFixture : ICollectionFixture<MovieReservationWebApplicationFactory>;
 }
