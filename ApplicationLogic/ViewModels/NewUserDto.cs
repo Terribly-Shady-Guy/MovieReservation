f0e872db691822b07ca93a0e56f47a96bd4e7ab9
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-12-01 08:59:08 -0600

    added a confirm password prop for creating a new user

--- ApplicationLogic/ViewModels/NewUserDto.cs
+++ ApplicationLogic/ViewModels/NewUserDto.cs
@@ -15,5 +15,7 @@ namespace ApplicationLogic.ViewModels
         [RegularExpression(@"^(?:(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%&*+=]))[\da-zA-Z!@#$%&*+=]{8,}$",
             ErrorMessage = "Password does not contain an upercase letter, lowercase letter, number, or special character, or is not at least 8 characters long.")]
         public required string Password { get; set; }
+        [Compare(nameof(Password), ErrorMessage = "The confirming password does not match the supplied one.")]
+        public required string ConfirmPassword { get; set; }
     }
 }
