01986bca2cb386e0bee0a070851d0fe39e04a751
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2025-12-01 09:10:56 -0600

    added a max length for password

--- ApplicationLogic/ViewModels/NewUserDto.cs
+++ ApplicationLogic/ViewModels/NewUserDto.cs
@@ -12,7 +12,7 @@ namespace ApplicationLogic.ViewModels
         public required string Email { get; set; }
         [MinLength(5)]
         public required string Username { get; set; }
-        [RegularExpression(@"^(?:(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%&*+=]))[\da-zA-Z!@#$%&*+=]{8,}$",
+        [RegularExpression(@"^(?:(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%&*+=]))[\da-zA-Z!@#$%&*+=]{8,100}$",
             ErrorMessage = "Password does not contain an upercase letter, lowercase letter, number, or special character, or is not at least 8 characters long.")]
         public required string Password { get; set; }
         [Compare(nameof(Password), ErrorMessage = "The confirming password does not match the supplied one.")]
