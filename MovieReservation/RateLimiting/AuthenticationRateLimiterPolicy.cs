e0acf61fec8fe722091207ac293fc50881457a4f
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2026-01-04 15:47:21 -0600

    moved rejected handler into a named method

--- MovieReservation/RateLimiting/AuthenticationRateLimiterPolicy.cs
+++ MovieReservation/RateLimiting/AuthenticationRateLimiterPolicy.cs
@@ -6,11 +6,24 @@ namespace MovieReservation.RateLimiting
 {
     public class AuthenticationRateLimiterPolicy : IRateLimiterPolicy<string>
     {
-        public Func<OnRejectedContext, CancellationToken, ValueTask>? OnRejected { get; } = async (rejectedContext, cancellationToken) =>
+        public Func<OnRejectedContext, CancellationToken, ValueTask>? OnRejected { get; } = HandleRejected;
+
+        public RateLimitPartition<string> GetPartition(HttpContext httpContext)
+        {
+            string clientIpAddress = GetClientIpAddress(httpContext);
+            return RateLimitPartition.GetSlidingWindowLimiter(clientIpAddress, _ => new SlidingWindowRateLimiterOptions
+            {
+                Window = TimeSpan.FromMinutes(10),
+                PermitLimit = 30,
+                SegmentsPerWindow = 10
+            });
+        }
+
+        private static async ValueTask HandleRejected(OnRejectedContext rejectedContext, CancellationToken cancellationToken)
         {
             Endpoint? rateLimitedEndpoint = rejectedContext.HttpContext.GetEndpoint();
             var rateLimitingAttribute = rateLimitedEndpoint?.Metadata.GetRequiredMetadata<EnableRateLimitingAttribute>();
-            
+
             string clientIpAddress = GetClientIpAddress(rejectedContext.HttpContext);
 
             var logger = rejectedContext.HttpContext.RequestServices.GetRequiredService<ILogger<AuthenticationRateLimiterPolicy>>();
@@ -19,7 +32,7 @@ namespace MovieReservation.RateLimiting
                 rateLimitingAttribute?.PolicyName ?? "Unknown policy",
                 rateLimitedEndpoint?.DisplayName ?? "Unknown endpoint",
                 rejectedContext.HttpContext.TraceIdentifier);
-            
+
             if (rejectedContext.Lease.TryGetMetadata(MetadataName.RetryAfter, out TimeSpan retryAfterTime))
             {
                 rejectedContext.HttpContext.Response.Headers.RetryAfter = ((int)retryAfterTime.TotalSeconds).ToString();
@@ -36,17 +49,6 @@ namespace MovieReservation.RateLimiting
 
             var detailsService = rejectedContext.HttpContext.RequestServices.GetRequiredService<IProblemDetailsService>();
             await detailsService.WriteAsync(detailsContext);
-        };
-
-        public RateLimitPartition<string> GetPartition(HttpContext httpContext)
-        {
-            string clientIpAddress = GetClientIpAddress(httpContext);
-            return RateLimitPartition.GetSlidingWindowLimiter(clientIpAddress, _ => new SlidingWindowRateLimiterOptions
-            {
-                Window = TimeSpan.FromMinutes(10),
-                PermitLimit = 30,
-                SegmentsPerWindow = 10
-            });
         }
 
         private static string GetClientIpAddress(HttpContext httpContext)
