21a86ff11ccaeeec99703ab3356a50d91b75f959
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2026-02-21 08:53:44 -0600

    edited mime types to use the static class ones

--- Tests/Unit/MoviePosterFileAttribute_IsValid.cs
+++ Tests/Unit/MoviePosterFileAttribute_IsValid.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationLogic.ValidationAttributes;
 using Microsoft.AspNetCore.Http;
+using System.Net.Mime;
 using System.Text;
 
 namespace Tests.Unit
@@ -7,12 +8,12 @@ namespace Tests.Unit
     public class MoviePosterFileAttribute_IsValid
     {
         [Theory]
-        [InlineData("valid-test-file-1.jpeg", 30 * 1024, "image/jpeg", true)]
-        [InlineData("valid-test-file-2.png", 30 * 1024, "image/png", true)]
-        [InlineData("invalid-test-file-1.txt", 30 * 1024, "text/plain", false)]
-        [InlineData("invalid-test-file-2.jpg", 11 * 1024 * 1024, "image/jpeg", false)]
-        [InlineData("invalid-test-file-mime.jpg", 11 * 1024, "text/plain", false)]
-        [InlineData("invalid-test-file.jpg.exe", 30 * 1024, "image/jpeg", false)]
+        [InlineData("valid-test-file-1.jpeg", 30 * 1024, MediaTypeNames.Image.Jpeg, true)]
+        [InlineData("valid-test-file-2.png", 30 * 1024, MediaTypeNames.Image.Png, true)]
+        [InlineData("invalid-test-file-1.txt", 30 * 1024, MediaTypeNames.Text.Plain, false)]
+        [InlineData("invalid-test-file-2.jpg", 11 * 1024 * 1024, MediaTypeNames.Image.Jpeg, false)]
+        [InlineData("invalid-test-file-mime.jpg", 11 * 1024, MediaTypeNames.Text.Plain, false)]
+        [InlineData("invalid-test-file.jpg.exe", 30 * 1024, MediaTypeNames.Image.Jpeg, false)]
         public void IsValid_WithFormFile_ReturnsExpected(string fileName, int fileSize, string contentType, bool expected)
         {
             byte[] fileBytes = Encoding.UTF8.GetBytes(fileName);
